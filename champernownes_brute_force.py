a78dc0e4876b053e0cad91494196dd95bdc2e2ee
Chris Aagaard <chrisnaagaard@gmail.com>
2017-05-14 16:52:17 -0700

    Ugly version to see where 'better' program was failing

---
+++ champernownes_brute_force.py
@@ -0,0 +1,13 @@
+# Solve Project Euler problem 40 by brute force
+
+champs = ''
+for i in range(0,1000000):
+  champs = champs + str(i)
+
+print(champs[1])
+print(champs[10])
+print(champs[100])
+print(champs[1000])
+print(champs[10000])
+print(champs[100000])
+print(champs[1000000])
